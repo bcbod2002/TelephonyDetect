e92fd2d8d9afaefdde611cbc5f81b89a25d564fe
Goston <bcbod2002@gmail.com>
2017-05-07 23:41:14 +0800

    Initial Commit

---
+++ TelephonyDetect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TelephonyDetect
+//
+//  Created by Goston on 07/05/2017.
+//  Copyright © 2017 Goston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
